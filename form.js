bfe96d000e16f82a2b4df7da7f7323db7ec69d74
rameshgyawali21452 <145563315+rameshgyawali21452@users.noreply.github.com>
2024-05-14 15:25:18 +0545

    Add files via upload

---
+++ form.js
@@ -0,0 +1,4 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent form submission
+    alert('Submitted Successfully!'); // show alert message
+});
